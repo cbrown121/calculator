9844625d84f06bde415c2f74d39913e57f559309
Chris <cbrown.cloud@gmail.com>
2025-04-11 12:00:10 -0700

    Add functions and input buttons

---
+++ calculator.js
@@ -0,0 +1,33 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(operater, num1, num2) {
+    switch (operater) {
+        case add:
+            return add(num1, num2);
+        case subtract:
+            return subtract(num1, num2);
+        case multiply:
+            return multiply(num1, num2);
+        case divide:
+            return divide(num1, num2);
+
+        default:
+            return "invalid";
+    }
+}
+
+console.log(operate(multiply, 3, 4));
