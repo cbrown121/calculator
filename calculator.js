72a6aa99397e85f4eea0a76ed0d202840832b186
Chris <cbrown.cloud@gmail.com>
2025-04-12 07:20:15 -0700

    Add selected buttons to the display

--- calculator.js
+++ calculator.js
@@ -30,4 +30,29 @@ function operate(operater, num1, num2) {
     }
 }
 
-console.log(operate(multiply, 3, 4));
+// Need to store the numbers in the display
+// First need to reference the selected buttons
+
+// button is selected
+// that button is added to the display
+const container = document.getElementById("container");
+const display = document.querySelector(".display");
+
+
+container.addEventListener('click', (event) => {
+    const isButton = event.target.nodeName === 'BUTTON';
+    if (!isButton) {
+      return;
+    }
+    if (event.target.textContent === "C") { // if the user is selecting the clear button, replace everything with a 0
+        display.textContent = "0";
+    } else if ((display.textContent === "0") && (event.target.id != ".")) { // if the display starts with a 0 and the next button is not a decimal, replace the 0
+        display.textContent = event.target.textContent;
+    } else if ((display.textContent.includes(".")) && (event.target.id === ".")) { //if the display already contains a decimal
+        // don't add to the display 
+        return;
+    } else {
+        display.textContent += event.target.textContent; // append the next number to the current number
+    }
+    console.dir(event.target.id);
+});
